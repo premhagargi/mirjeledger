eaaabe1ebbc8c40e076b4c30f26a2bdb58ca73d9
Pratik Mirje <mirjeteadepot@gmail.com>
2026-02-25 07:36:52 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4435788400-d0d8b",
+  "appId": "1:771675115986:web:949cb60698017d415a780b",
+  "apiKey": "AIzaSyB2P5aPj9mx93cVo9MniJXcW8kE6KJ4ymU",
+  "authDomain": "studio-4435788400-d0d8b.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "771675115986"
+};
